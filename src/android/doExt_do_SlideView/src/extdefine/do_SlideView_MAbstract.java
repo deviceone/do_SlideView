a22007fd762c38ea846183c1234f1ce1b7b126f2
zhuozuoying <zhuozuoying@163.com>
2015-04-10 15:12:05 +0800

    android提交slideview

---
+++ src/android/doExt_do_SlideView/src/extdefine/do_SlideView_MAbstract.java
@@ -0,0 +1,20 @@
+package extdefine;
+
+import core.object.DoUIModule;
+
+
+public abstract class do_SlideView_MAbstract extends DoUIModule{
+
+	protected do_SlideView_MAbstract() throws Exception {
+		super();
+	}
+	
+	/**
+	 * 初始化
+	 */
+	@Override
+	public void onInit() throws Exception{
+        super.onInit();
+        //注册属性
+	}
+}
---
+++ src/android/doExt_do_SlideView/src/extimplement/do_SlideView_Model.java
@@ -0,0 +1,15 @@
+package extimplement;
+
+import extdefine.do_SlideView_MAbstract;
+
+/**
+ * 自定义扩展组件Model实现，继承do_SlideView_MAbstract抽象类；
+ *
+ */
+public class do_SlideView_Model extends do_SlideView_MAbstract {
+
+	public do_SlideView_Model() throws Exception {
+		super();
+	}
+	
+}
